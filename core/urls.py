485a585ec15eb4fb73f754847f01d9846d6395f4
Adebayo Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2025-11-13 15:39:58 +0100

    Change API path to root for blog URLs

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('blog.urls')),  # ✅ includes router and endpoints
+    path('', include('blog.urls')),  # ✅ includes router and endpoints
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
